01f27b00d44feede8dd6f485562668dbc0e30d26
lukefrostwalker <lukefrostwalker@outlook.com>
2022-10-28 12:45:38 +0800

    second commit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
+
+function toMicrometer() {
+    let inputValue = document.getElementById("inches").value;
+    document.getElementById("output").innerHTML = inputValue * 25400;
+    document.getElementById("output_unit").innerHTML = "Micrometer";
+}
 function toMillimeter() {
     let inputValue = document.getElementById("inches").value;
     document.getElementById("output").innerHTML = inputValue * 25.4;
@@ -13,3 +19,9 @@ function toMeter() {
     document.getElementById("output").innerHTML = inputValue / 39.37;
     document.getElementById("output_unit").innerHTML = "Meter";
 }
+function toKilometer() {
+    let inputValue = document.getElementById("inches").value;
+    document.getElementById("output").innerHTML = inputValue / 39370;
+    document.getElementById("output_unit").innerHTML = "Kilometer";
+}
+@media
