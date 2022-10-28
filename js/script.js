7580f86bb773ae9cf86794a61d068d21d7c033b3
lukefrostwalker <lukefrostwalker@outlook.com>
2022-10-28 15:21:08 +0800

    update

--- js/script.js
+++ js/script.js
@@ -1,26 +1,36 @@
 
 function toMicrometer() {
     let inputValue = document.getElementById("inches").value;
-    document.getElementById("output").innerHTML = inputValue * 25400;
+    const micro = 25400
+    const ans = inputValue * micro
+    document.getElementById("output").innerHTML = ans;
     document.getElementById("output_unit").innerHTML = "Micrometer";
 }
 function toMillimeter() {
     let inputValue = document.getElementById("inches").value;
-    document.getElementById("output").innerHTML = inputValue * 25.4;
+    const milli = 25.4
+    const ans = inputValue * milli
+    document.getElementById("output").innerHTML = ans;
     document.getElementById("output_unit").innerHTML = "Millimeter";
 }
 function toCentimeter() {
     let inputValue = document.getElementById("inches").value;
-    document.getElementById("output").innerHTML = inputValue * 2.54;
+    const centi = 2.54
+    const ans = inputValue * centi
+    document.getElementById("output").innerHTML = ans;
     document.getElementById("output_unit").innerHTML = "Centimeter";
 }
 function toMeter() {
     let inputValue = document.getElementById("inches").value;
-    document.getElementById("output").innerHTML = inputValue / 39.37;
+    const meter = 39.37
+    const ans = inputValue * meter
+    document.getElementById("output").innerHTML = ans;
     document.getElementById("output_unit").innerHTML = "Meter";
 }
 function toKilometer() {
     let inputValue = document.getElementById("inches").value;
-    document.getElementById("output").innerHTML = inputValue / 39370;
+    const kilo = 39370
+    const ans = inputValue * kilo
+    document.getElementById("output").innerHTML = ans;
     document.getElementById("output_unit").innerHTML = "Kilometer";
 }
